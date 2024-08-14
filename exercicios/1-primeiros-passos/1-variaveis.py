c7220fd2210abb1ecbf7b0d4b6efb1c4d2713a8d
crescencio-resource <escritor.crescencio@gmail.com>
2024-08-14 09:52:11 -0300

    Resolução de exercicio

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,23 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+book = 'com amor simon'
+
+vendedor ="Lucareli"
+
+adaptacao = ''' É assim
+que
+acaba'''
+
+
+
+
 
 # Declare variáveis de cada tipo numérico
+pokedex = '301'
 
+temperatura = '34.5'
 
 # Declare uma variável do tipo booleano
+sim = "true"
+nao = "false"
